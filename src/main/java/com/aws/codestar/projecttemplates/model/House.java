1313a632dfb13fbfc91096ad28af2a78d46c6aeb
KU2231 <KU2231@NLLR4000291512.solon.prd>
2018-06-07 06:48:56 +0200

    update getters and setters

--- src/main/java/com/aws/codestar/projecttemplates/model/House.java
+++ src/main/java/com/aws/codestar/projecttemplates/model/House.java
@@ -29,6 +29,23 @@ public class House implements Serializable {
 		// TODO Auto-generated method stub
 		return this.id +this.name;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
     
+	
     
 }
